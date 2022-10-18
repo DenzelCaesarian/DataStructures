2a46b143b847de9d94a402c05e3183163bed18ee
Denzel Caesarian Iswanto Suhardjo <101154087+DenzelCaesarian@users.noreply.github.com>
2022-10-18 12:36:13 +0700

    Update InfixToPostfix.cpp

--- InfixToPostfix.cpp
+++ InfixToPostfix.cpp
@@ -1,10 +1,3 @@
-/* 
-2440021376_Denzel Caesarian Iswanto Suhardjo
-LI01 - Data Structure (COMP6048)
-
-Topic   : Infix Evaluation (Infix to Postfix)
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
